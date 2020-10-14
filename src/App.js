90670d5fee9e904416bc4f22df04eef5fa0977fb
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-10-14 22:43:25 +0700

    feat(sidebar): constract sidebar with toggle sidebar button and sidebar dummy content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import SideBar from './components/sidebar/side-bar.component';
+
 function App() {
   return (
     <div className="App">
+      <SideBar/>
     </div>
   );
 }
---
+++ src/components/sidebar/side-bar.component.jsx
@@ -0,0 +1,103 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+import './side-bar.styles.scss';
+
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isBtnOpen: true
+    }
+
+    this.toggleSidebarOpenbtn = this.toggleSidebarOpenbtn.bind(this);  
+  }
+
+  toggleSidebarOpenbtn() {
+    this.setState(prevState => ({
+      isBtnOpen: !prevState.isBtnOpen
+    }));
+  }
+
+  render() {
+    return (
+      <div className='side-bar-container'>
+        <div 
+        className={`sidebar sticky-sidebar ${this.state.isBtnOpen ? 'l-toggle-on':'l-toggle-off'}`} >
+          <div className='flex-mb-500'>
+            <div className='button-shadow'>BLOG</div>
+            <div className="sub-bracket-collection">
+              <div className='button-table-of-content parent-table-of-content'>
+                  <span className='text-table-of-content'>SEMINAR &amp; WORKSHOP</span>
+              </div>
+              <div className='collection-item'>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+              </div>
+            </div>
+          
+            <div className="sub-bracket-collection">
+              <div className='button-table-of-content parent-table-of-content'>
+                  <span className='text-table-of-content'>SEMINAR &amp; WORKSHOP</span>
+              </div>
+              <div className='collection-item'>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+              </div>
+            </div>
+          
+            <div className="sub-bracket-collection">
+              <div className='button-table-of-content parent-table-of-content'>
+                  <span className='text-table-of-content'>SEMINAR &amp; WORKSHOP</span>
+              </div>
+              <div className='collection-item'>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+                <a href='#' className='button-table-of-content item-table-of-content'>
+                  <span className='text-table-of-content'>Webinar E-Learning &amp; IOT Platform Batch 2</span>
+                </a>
+              </div>
+            </div>
+          
+          </div>
+        </div>
+        <div 
+          className={`openbtn sticky-openbtn ${this.state.isBtnOpen ? 'ml-toggle-on':'ml-toggle-off'}`} 
+          id='buttonSlider-js' 
+          onClick={() => this.toggleSidebarOpenbtn()}>
+            <span>{this.state.isBtnOpen ? '<':'>'}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
