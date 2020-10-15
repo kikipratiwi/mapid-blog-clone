88dd9d51c2fd6c9f604c73c1306696d105a009df
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-10-15 11:35:22 +0700

    feat(hero-title): construct hero-title with hero-image and blog title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import SideBar from './components/sidebar/side-bar.component';
+import SideBar from './components/sidebar/sidebar.component';
+import HeroTitle from './components/hero-title/hero-title.component';
 
 function App() {
   return (
     <div className="App">
       <SideBar/>
+      <HeroTitle/>
     </div>
   );
 }
