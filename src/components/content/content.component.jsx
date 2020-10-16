223b6cc448403c34b5f9d2ce86b85438ba915d54
Kiki <kiki.pratiwi.tif17@polban.ac.id>
2020-10-16 20:07:53 +0700

    feat(rich-rext): add draft-js to display rich text

--- src/components/content/content.component.jsx
+++ src/components/content/content.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CompositeDecorator, convertFromRaw, Editor, EditorState } from 'draft-js';
 
 import HeroTitle from '../../components/hero-title/hero-title.component';
 import Author from '../../components/author/author.component';
@@ -10,16 +11,43 @@ class Content extends React.Component {
     super(props);
 
     this.state = {
-      defaultUserImageProfile: 'https://doc.mapid.io/static/media/profile_pic_comment.2a6f2a32.svg'
+      defaultUserImageProfile: 'https://doc.mapid.io/static/media/profile_pic_comment.2a6f2a32.svg',
+      defaultHeroImage: 'https://s3.us-west-2.amazonaws.com/machrus/doc_pictures/1601430916.png'
     }
   }
 
+  findLinkEntities = (contentBlock, callback, contentState) => {
+    contentBlock.findEntityRanges(
+      (character) => {
+        const entityKey = character.getEntity();
+        return (
+          entityKey !== null &&
+          contentState.getEntity(entityKey).getType() === 'LINK'
+        );
+      },
+      callback
+    );
+  }
+
+  compositeDecorator = new CompositeDecorator([{
+    strategy: this.findLinkEntities,
+    component: Content
+  }]);
+
+  convertToEditorState = (editorContent) => {    
+    const contentState = convertFromRaw(JSON.parse(editorContent.editorState));
+    const editorState = EditorState.createWithContent(contentState, this.compositeDecorator);
+    return editorState;
+  };
+
   render() {
     const contentData = this.props.contentData;
+    const editorState = this.convertToEditorState(contentData);
+
     return (
       <div className="mb-200">
         <HeroTitle 
-          imageUrl={'https://s3.us-west-2.amazonaws.com/machrus/doc_pictures/1598604952.jpeg'} 
+          imageUrl={this.state.defaultHeroImage} 
           title={this.props.contentData.title} />
         <div className="content-container">
           {this.props.contentData ? (
@@ -34,7 +62,7 @@ class Content extends React.Component {
                 date={contentData.date}
               />
               <div className="content">
-                <p>{contentData.editorState}</p>
+                <Editor editorState={editorState} readOnly={true} />
               </div>
             </>
           ) : (
